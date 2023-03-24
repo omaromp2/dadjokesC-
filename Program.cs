7a06ac20f82f7a150259b422579a51721ec14dca
oponce <oponce@multisystems.com>
2023-03-24 16:47:13 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using Newtonsoft.Json; 
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        // Greeting 
+        Console.WriteLine("Hello, I shall fetch you a joke!");
+
+        // Http Client 
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+        var result = client.GetAsync("https://icanhazdadjoke.com").Result;
+
+        //Check resp 
+        if (result.IsSuccessStatusCode)
+        {
+            var dadJokeJson = result.Content.ReadAsStringAsync().Result;
+            dynamic dadJokeData = JsonConvert.DeserializeObject(dadJokeJson);
+            string dadjoke = dadJokeData.joke;
+
+            Console.WriteLine(dadjoke); 
+
+        }
+        else
+        {
+            Console.WriteLine("Oops! Unable to joke at the moment");
+        }
+
+
+
+    }
+
+}
